87d4b8d735642558c1d92e43aa8439f44676665a
W4r10ck423 <warlock423@gmail.com>
2021-10-28 20:31:53 -0600

    Update unattended_install.sh

--- unattended_install.sh
+++ unattended_install.sh
@@ -7,7 +7,7 @@ installerURL=$(curl -sL https://api.github.com/repos/karakun/OpenWebStart/releas
 installerVersion=$(echo $installerURL | cut -d\/ -f8)
 installerFile=$(echo $installerURL | cut -d\/ -f9)
 echo "[INFO] The current installer version is $installerVersion"
-osascript -e 'tell app "Terminal" to display dialog "DrsBee: Por favor espera mientras se instalan los componentes requeridos (Este proceso puede tardar varios minutos)"'
+osascript -e 'display notification "(Este proceso puede tardar algunos minutos)" with title "DrsBee" subtitle "Por favor espere mientras se instalan los componentes requeridos" sound name "Submarine"'
 if test -f "$installerFile"; then
 	echo "[INFO] You have already downloaded the latest installer file... Now downloading installation config file..."
 else
