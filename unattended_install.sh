0d43cb8776424ea6874458936a31a01fbd31a69a
W4r10ck423 <warlock423@gmail.com>
2022-02-14 13:50:43 -0600

    Update unattended_install.sh

--- unattended_install.sh
+++ unattended_install.sh
@@ -52,7 +52,10 @@ fi
 echo "[INFO] Running app for the first time..."
 killall Finder
 killall firefox 
-nohup /Applications/Firefox.app/Contents/MacOS/firefox "$jnlpFile" "https://dev.drsbee.com/es-CR/Account/Login" &
+#nohup /Applications/Firefox.app/Contents/MacOS/firefox "$jnlpFile" "https://dev.drsbee.com/es-CR/Account/Login" &
+curl -L -o DrsBee.jnlp "$jnlpFile"
+nohup open "/Applications/OpenWebStart/OpenWebStart javaws.app" DrsBee.jnlp --args --Xoffline &
 echo "[INFO] Ejecting volumes"
 hdiutil detach /Volumes/OpenWebStart
 hdiutil detach /Volumes/DrsBeeWebStart
+
