8357f0e129eccdca3f44f070bd724557b0660eab
W4r10ck423 <warlock423@gmail.com>
2022-07-14 11:12:56 -0600

    Update launch_jnlp.sh

--- launch_jnlp.sh
+++ launch_jnlp.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 # ApoloLab's JNLP Launcher for OSX
 # Developed by: Dani3l Murill0
+osascript -e 'display notification "Por favor, espere..." with title "DrsBee" subtitle "Inicializando módulo de firma digital" sound name "Submarine"'
 jnlpURL="https://dev.drsbee.com/es-CR/Account/DoSignatureLogin?contextData=QVNQLk5FVF9TZXNzaW9uSWQ9QzNBOTFFMTBBMzE0RTEwREE5MTZDMUQx";
 curl -o "DrsbeeSignatureLauncher.jnlp" -L "$jnlpURL"
 xattr com.apple.quarantine "DrsbeeSignatureLauncher.jnlp"
