617a119e92d498de8c641b56b52861d886ce7478
Daniel <dani3l.murill0.506@gmail.com>
2023-11-15 20:57:58 -0600

    script fixes

--- uninstall_drsbeewebstart.sh
+++ uninstall_drsbeewebstart.sh
@@ -1,2 +1,12 @@
 #! /bin/bash
 sudo rm -rf $HOME/.cache/icedtea-web/* /Applications/websigner-launcher.app /Applications/OpenWebStart/ /Applications/IDProtectClient/ $HOME/.config/icedtea-web /Library/Application\ Support/Athena/ /usr/local/lib/libASEP11.dylib 
+if [ -d "/Library/Java/JavaVirtualMachines/jdk1.8.0_361.jdk" ]; then
+    sudo rm -rf /Library/Java/JavaVirtualMachines/jdk1.8.0_361.jdk
+fi
+if [ -f "/Library/LaunchDaemons/com.apololab.handlejnlp.plist" ]; then
+    sudo launchctl unload /Library/LaunchDaemons/com.apololab.handlejnlp.plist
+    sudo rm -rf /Library/LaunchDaemons/com.apololab.handlejnlp.plist
+fi
+if [ -d "$HOME/.local/share/apololab" ]; then
+    sudo rm -rf $HOME/.local/share/apololab
+fi
