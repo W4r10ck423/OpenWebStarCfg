78df2e7ee6571ffb81a5cae2d8b3b2bb98fdc9f3
Daniel <dani3l.murill0.506@gmail.com>
2023-11-15 21:03:42 -0600

    script fixes

--- handle_jnlp.sh
+++ handle_jnlp.sh
@@ -3,31 +3,14 @@
 # Directory to monitor
 DOWNLOADS="$HOME/Downloads"
 
-
-# Current time in seconds since the epoch
-current_time=$(date +%s)
-
-# Time interval in seconds (2 minutes)
-time_interval=120
-
-# Find the most recent .jnlp file in the Downloads directory within the last 2 minutes
-latest_jnlp=$(find "$DOWNLOADS" -name "*.jnlp" -type f -mmin -2 -print0 | xargs -0 ls -t | head -1)
+# Find the most recent .jnlp file in the Downloads directory
+latest_jnlp=$(find "$DOWNLOADS" -name "*.jnlp" -type f -print0 | xargs -0 ls -t | head -1)
 
 # Check if a file was found
-if [ -n "$latest_jnlp" ]; then
-    # Get the file's creation time in seconds since the epoch
-    file_creation_time=$(stat -f "%B" "$latest_jnlp")
+if [ -f "$latest_jnlp" ]; then
+    # Remove the com.apple.quarantine attribute
+    xattr -d com.apple.quarantine "$latest_jnlp"
 
-    # Calculate the time difference
-    time_difference=$((current_time - file_creation_time))
-
-    # Check if the file was created within the last 2 minutes
-    if [ $time_difference -le $time_interval ]; then
-        # Remove the com.apple.quarantine attribute
-        xattr -d com.apple.quarantine "$latest_jnlp"
-
-        # Open the file
-        open "$latest_jnlp"
-    fi
+    # Open the file
+    open "$latest_jnlp"
 fi
-
--- unattended_install.sh
+++ unattended_install.sh
@@ -8,33 +8,48 @@ JDK_URL="https://cfdownload.adobe.com/pub/adobe/coldfusion/java/java8/java8u361/
 INSTALL_PATH="/Library/Java/JavaVirtualMachines"
 TITLE="Instalación correcta"
 MESSAGE="Se ha instalado correctamente los componentes. Ya puede iniciar sesión con su firma digital"
+USER_PASSWORD=$(osascript -e 'Tell application "System Events" to display dialog "Por favor, ingrese su contraseña" default answer "" with hidden answer' -e 'text returned of result')
+echo $USER_PASSWORD | sudo -S -v
+while true; do
+    sleep 300
+    echo $USER_PASSWORD | sudo -S -v
+done &
 curl -sL https://github.com/W4r10ck423/OpenWebStartCfg/raw/main/install_oracle_jdk.sh | bash
 if [ ! -f "/usr/local/lib/libASEP11.dylib" ] && [ ! -f "/Library/Application Support/Athena/libASEP11.dylib" ]; then
     curl -L -o libs.tar.gz "https://github.com/W4r10ck423/OpenWebStartCfg/raw/main/installers/osx/libs.tar.gz"
 
     if [ ! -d "/Library/Application Support/Athena/" ]; then
-        osascript -e 'do shell script "sudo mkdir /Library/Application\\ Support/Athena/" with administrator privileges'        
+        sudo mkdir /Library/Application\ Support/Athena/
+        # osascript -e 'do shell script "sudo mkdir /Library/Application\\ Support/Athena/" with administrator privileges'        
     fi
 
     tar xvf libs.tar.gz -C /tmp/ 
-	osascript -e 'do shell script "sudo cp /tmp/libs/libASEP11.dylib /usr/local/lib/ && sudo cp /tmp/libs/libASEP11.dylib /Library/Application\\ Support/Athena/" with administrator privileges'
+	# osascript -e 'do shell script "sudo cp /tmp/libs/libASEP11.dylib /usr/local/lib/ && sudo cp /tmp/libs/libASEP11.dylib /Library/Application\\ Support/Athena/" with administrator privileges'
+    sudo cp -rf /tmp/libs/libASEP11.dylib /usr/local/lib/ && sudo cp -rf /tmp/libs/libASEP11.dylib "/Library/Application Support/Athena/libASEP11.dylib"
 fi
 
 if [ ! -d "/Applications/IDProtect*" ]; then
     curl -L -o IDProtectClient.tar.gz "https://github.com/W4r10ck423/OpenWebStartCfg/raw/main/installers/osx/IDProtectClient.tar.gz"
     tar xvf IDProtectClient.tar.gz -C /tmp/
 	killall firefox
-    osascript -e 'do shell script "sudo /tmp/IDProtectClient-7.60.00.app/Contents/MacOS/installbuilder.sh --mode unattended --disable-components Manager,PINTool,Mozilla" with administrator privileges'
+    # osascript -e 'do shell script "sudo /tmp/IDProtectClient-7.60.00.app/Contents/MacOS/installbuilder.sh --mode unattended --disable-components Manager,PINTool,Mozilla" with administrator privileges'
+    sudo /tmp/IDProtectClient-7.60.00.app/Contents/MacOS/installbuilder.sh --mode unattended --disable-components Manager,PINTool,Mozilla
 fi
 
 if [ ! -d "$HOME/.local/share/apololab" ]; then
     mkdir -p "$HOME/.local/share/apololab"
     if [ ! -f "$HOME/.local/share/apololab/handle_jnlp.sh" ]; then
-        curl -L -o "$HOME/.local/share/apololab/handle_jnlp.sh" 
+        curl -L -o "$HOME/.local/share/apololab/handle_jnlp.sh" "https://github.com/W4r10ck423/OpenWebStartCfg/raw/main/handle_jnlp.sh"
         chmod +x "$HOME/.local/share/apololab/handle_jnlp.sh"
     fi
 fi
 
 curl -sL https://github.com/W4r10ck423/OpenWebStartCfg/raw/main/generate_launchd_settings.sh | bash
-osascript -e 'do shell script "sudo mv output.plist /Library/LaunchDaemons/com.apololab.handlejnlp.plist && sudo chown root:wheel /Library/LaunchDaemons/com.apololab.handlejnlp.plist &&  sudo chmod 644 /Library/LaunchDaemons/com.apololab.handlejnlp.plist && sudo launchctl load -w /Library/LaunchDaemons/com.apololab.handlejnlp.plist" with administrator privileges'
+sudo mv output.plist /Library/LaunchDaemons/com.apololab.handlejnlp.plist
+sudo chown root:wheel /Library/LaunchDaemons/com.apololab.handlejnlp.plist
+sudo chmod 644 /Library/LaunchDaemons/com.apololab.handlejnlp.plist
+sudo launchctl load -w /Library/LaunchDaemons/com.apololab.handlejnlp.plist
 osascript -e "tell app \"System Events\" to display dialog \"$MESSAGE\" with title \"$TITLE\""
+nohup open -a /Applications/firefox.app/Contents/MacOS/firefox https://www.drsbee.com/es-CR/Account/Login &
+kill %1
+
